54979c7d5b11d59cd73c73db431a419a5c66fdb6
zhenzhitianxia <kldehaizi@163.com>
2018-05-29 15:20:58 +0800

    第一次提交

---
+++ webserver/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	"io"
+)
+
+func firstPage(w http.ResponseWriter, r *http.Request)  {
+	io.WriteString(w, "<p>你好，我是golang</p>");
+}
+
+func main() {
+	http.HandleFunc("/", firstPage);
+	http.ListenAndServe(":8000", nil);
+
+}
